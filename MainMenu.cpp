76d745bb1a7b69336c9f2fb29582ee017f5f55a7
georgemusson79 <149239702+georgemusson79@users.noreply.github.com>
2023-11-04 14:08:46 +0000

    Auto stash before merge of "main" and "origin/main"

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -1,8 +1,9 @@
 #include "MainMenu.h"
 #include "globals.h"
+#include <SDL.h>
 
 MainMenu::MainMenu() {
-
+	this->bg = IMG_LoadTexture(renderer, "assets//images//bg.png");
 }
 MainMenu::~MainMenu() {
 	SDL_DestroyTexture(this->bg);
@@ -10,4 +11,5 @@ MainMenu::~MainMenu() {
 
 void MainMenu::update() {
 	if (!this->active) return;
+	SDL_RenderCopy(renderer, bg, NULL, NULL);
 }
--- main.cpp
+++ main.cpp
@@ -3,18 +3,19 @@
 
 int main() {
 	SDL_Init(SDL_INIT_EVERYTHING);
+	IMG_Init(IMG_INIT_PNG);
 	window = SDL_CreateWindow("noughts and crosses", 200, 200, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	mainMenu = new MainMenu();
 
     readTestingGameState();
 
 	while (running) {
 		SDL_RenderClear(renderer);
 		if (gameState == MAIN_MENU) {
-
-		} else if (gameState == MAIN_GAME) {
-
-        }
+			if (mainMenu != nullptr) mainMenu->update();
+		}
 		handleMainEvents();
 		SDL_RenderPresent(renderer);
 	}
