dab51305b627cdd0ad42162bb2617455339ad39f
georgemusson79 <149239702+georgemusson79@users.noreply.github.com>
2023-11-04 17:52:21 +0000

    updated board thing to make boards

--- board.cpp
+++ board.cpp
@@ -1,4 +1,13 @@
 #include "board.h"
-Board::Board(int depth) {
+Board::Board(int depth) : depth(depth) {
+	if (this->depth == 0) this->isBase == true;
+}
 
+void Board::generateBoard() {
+	if (this->isBase) {
+		this->board = std::vector<char>(9, '-');
+	}
+	else {
+		this->boardOfBoards = std::vector<Board>(9, Board(depth - 1));
+	}
 }
--- board.h
+++ board.h
@@ -1,9 +1,10 @@
 #include <vector>
 class Board {
 public:
-	int depth;
-	std::vector<std::vector<char>> board = {};
-	std::vector<std::vector<Board>> boardOfBoards = {};
+	int depth = 0;
+	bool isBase = true; //is board to play noughts and crosses in and not board containing more boards
+	std::vector<char> board = {};
+	std::vector<Board> boardOfBoards = {};
 	Board(int depth);
-	void generateBoard(int depth);
+	void generateBoard();
 };
