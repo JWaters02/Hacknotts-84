af8ad927ada3349133778a14299bedcaf6b14f91
JWaters02 <watersjoshua2002@gmail.com>
2023-11-04 18:15:06 +0000

    button work

--- button.cpp
+++ button.cpp
@@ -8,44 +8,41 @@ bool Button::checkCollision(int X, int Y) {
 	return false;
 }
 
-void Button::setImg(std::string imgPath) {
-	//sets background image for button, if 
-	if (imgPath == "") return;
+void Button::setImg(const std::string& imgPath) {
+	if (imgPath.empty()) return;
 	if (this->img != nullptr) SDL_DestroyTexture(this->img);
 	this->img = IMG_LoadTexture(renderer, imgPath.c_str());
 }
 
-Button::Button(int x, int y, int w, int h,std::string imgPath, std::string text) : x(x), y(y), w(w), h(h) {
+Button::Button(int x, int y, int w, int h,
+               TTF_Font* font,
+               const std::string& imgPath,
+               const std::string& text,
+               SDL_Color textColour):
+               x(x), y(y), w(w), h(h), mFont(font) {
 	this->setImg(imgPath);
-    //this->setText(text, RED);
+    this->setText(text, textColour);
 }
 
 Button::~Button() {
 	if (this->img != nullptr) SDL_DestroyTexture(this->img);
-
+    if (this->text != nullptr) SDL_DestroyTexture(this->text);
 }
 
 void Button::renderButton() {
 	SDL_Rect renderPos = { x,y,w,h };
-	SDL_RenderCopy(renderer, this->img, NULL, &renderPos);
+	SDL_RenderCopy(renderer, this->img, nullptr, &renderPos);
 }
 
-void Button::setText(std::string text, SDL_Color textColor) {
-    // First, clean up any existing texture
-    if (img != nullptr) {
-        SDL_DestroyTexture(img);
-        img = nullptr;
-    }
-
-    // Render the text to a surface
-    SDL_Surface* textSurface = TTF_RenderText_Solid(mFont, text.c_str(), textColor);
+void Button::setText(const std::string& buttonText, SDL_Color textColor) {
+    if (buttonText.empty()) return;
+    SDL_Surface* textSurface = TTF_RenderText_Solid(mFont, buttonText.c_str(), textColor);
     if (textSurface == nullptr) {
-        SDL_Log("Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError());
+        SDL_Log("Unable to render buttonText surface! SDL_ttf Error: %s\n", TTF_GetError());
     } else {
-        // Create texture from surface pixels
-        img = SDL_CreateTextureFromSurface(renderer, textSurface);
-        if (img == nullptr) {
-            SDL_Log("Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError());
+        this->text = SDL_CreateTextureFromSurface(renderer, textSurface);
+        if (this->text == nullptr) {
+            SDL_Log("Unable to create texture from rendered buttonText! SDL Error: %s\n", SDL_GetError());
         }
 
         // Get the dimensions of the button
